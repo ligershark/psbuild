472bfaeadee969359d00ad6df2c120ec72d6bf93
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-01-04 13:42:05 -0800

    initial commit

---
+++ src/psbuild.psm1
@@ -0,0 +1,58 @@
+﻿<#
+.SYNOPSIS  
+	This module will help to use msbuild from powershell.
+    When you import this module the msbuild alias will be set
+#>
+
+
+
+<#
+.SYNOPSIS  
+	This will return the path to msbuild.exe. If the path has not yet been set
+	then the highest installed version of msbuild.exe will be returned.
+#>
+function Get-MSBuild{
+	$path = $defaultMSBuildPath
+
+	if(!$path){
+	    $path =  Get-ChildItem "hklm:\SOFTWARE\Microsoft\MSBuild\ToolsVersions\" | 
+				    Sort-Object {$_.Name} | 
+				    Select-Object -First 1 | 
+				    Get-ItemProperty -Name MSBuildToolsPath |
+				    Select -ExpandProperty MSBuildToolsPath
+        
+        $path = (Join-Path -Path $path -ChildPath 'msbuild.exe')
+	}
+
+    return Get-Item $path
+}
+<#
+.SYNOPSIS  
+	This has two purposes:
+        1. Create the msbuild alias
+        2. Users can specific a specific msbuild.exe which should be used
+#>
+function Set-MSBuild{
+    param(
+        [Parameter(ValueFromPipeline=$true)]
+        $msbuildPath = (Get-MSBuild)
+        #[switch]
+        #$setAlias = ($true),
+
+        #[switch]
+        #$setAsDefault = $false
+    )
+
+    process{
+            'Updating msbuild alias to point to [{0}]' -f $msbuildPath | Write-Verbose
+            Set-Alias msbuild $msbuildPath
+                
+            'Updating defalut msbuild.exe to point to [{0}]' -f $msbuildPath | Write-Verbose
+            $script:defaultMSBuildPath = $msbuildPath
+    }
+}
+
+# begin script portions
+[string]$script:defaultMSBuildPath = $null
+# call this once to ensure the alias is set
+Get-MSBuild | Set-MSBuild
---
+++ src/test.ps1
@@ -0,0 +1,24 @@
+function Get-ScriptDirectory
+{
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value
+    Split-Path $Invocation.MyCommand.Path
+}
+
+$scriptDir = ((Get-ScriptDirectory) + "\")
+$moduleName = 'psbuild'
+$modulePath = (Join-Path -Path $scriptDir -ChildPath ("{0}.psm1" -f $moduleName))
+
+if(Test-Path $modulePath){
+    "Importing psbuild module from [{0}]" -f $modulePath | Write-Verbose
+
+    if((Get-Module $moduleName)){
+        Remove-Module $moduleName
+    }
+    
+    Import-Module $modulePath -PassThru -DisableNameChecking | Out-Null
+}
+else{
+    'Unable to find pshelpers module at [{0}]' -f $modulePath | Write-Error
+	return
+}
+
