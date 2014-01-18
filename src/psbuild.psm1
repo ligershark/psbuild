48afabcbbe610013130b25e2a06f035a113d314e
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-01-17 22:45:51 -0800

    fixing a minor bug in new-project

--- src/psbuild.psm1
+++ src/psbuild.psm1
@@ -393,7 +393,7 @@ function New-Project{
         $newProj = [Microsoft.Build.Construction.ProjectRootElement]::Create()
 
         if($filePath){
-            Save-Project $newProj
+            Save-Project -project $newProj -filePath $filePath | Out-Null
         }
 
         return $newProj
