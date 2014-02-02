054375d79f54b1f3ac74e3f590d12bba9274f7a1
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2014-02-01 21:18:45 -0800

    updating test case

--- tests/invoke-msbuild.Tests.ps1
+++ tests/invoke-msbuild.Tests.ps1
@@ -6,21 +6,29 @@ $importPsbuild = (Join-Path -Path $here -ChildPath 'import-psbuild.ps1')
 Describe 'invoke-msbuild test cases' {
     
     $script:tempProj = 'invoke-msbuild\temp.proj'
-    $script:tempProjContent = @'
+    $script:genPath = 'invoke-msbuild\generated.txt'
+    $script:tempProjContent = @"
 <?xml version="1.0" encoding="utf-8"?>
 <Project xmlns="http://schemas.microsoft.com/developer/msbuild/2003" DefaultTargets="Demo" ToolsVersion="4.0">
 
   <Target Name="Demo">
-    <Message Text="Inside demo target of file [$(MSBuildProjectFile)]" Importance="high"/>
+    <Message Text="Inside demo target of file" Importance="high"/>
+    <WriteLinesToFile File="$TestDrive\$script:genPath"  Lines="from-unit test"/>
   </Target>
 
 </Project>
-'@
-    
-    Setup -File -Path 'invoke-msbuild\temp.proj' -Content 'ff'
+"@
 
-    It "in the new file" {
+    $global:PSBuildSettings.BuildMessageEnabled = $false
+
+    Setup -File -Path 'invoke-msbuild\temp.proj' -Content $script:tempProjContent
+
+    It "ensure the project is invoked" {
         $path = ("$TestDrive\{0}" -f $script:tempProj)
+
+        Copy-Item $path 'c:\temp\msbuild\genproj.proj'
         $path | Should Exist
+        Invoke-MSBuild $path
+        "$TestDrive\$genPath" | Should Exist
     }
 }
